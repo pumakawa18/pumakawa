3da395baaa91101c8482c557a6bbc74799504d27
kingintheno <morebeni08@gmail.com>
2026-07-03 13:58:04 -0300

    modificaciones en controladores put

--- controladores.js
+++ controladores.js
@@ -1,39 +1,39 @@
 import { usuariosFicticios } from './basedatos.js';
 
-export const getdata = async(req,res) => {
+export const getdata = async (req, res) => {
   res.json(usuariosFicticios);
 }
 
-export const postdata = async(req, res) => {
-  const {nombre, mail, contrasena} = req.body;
-  const newUser = {id: usuariosFicticios.length + 1, nombre, mail, contrasena}
-  console.log("User created successfully!!", {newUser})
+export const postdata = async (req, res) => {
+  const { nombre, mail, contrasena } = req.body;
+  const newUser = { id: usuariosFicticios.length + 1, nombre, mail, contrasena }
+  console.log("User created successfully!!", { newUser })
   res.status(201).json(newUser);
 }
 
-// apapa pipipi
-
-export const putdata = async(req, res) => {
+export const putdata = async (req, res) => {
   const userId = parseInt(req.params.id);
   const userIndex = usuariosFicticios.findIndex(
     user => user.id === userId
   )
-  if (userIndex === -1){
-    return res.status(201).json({message: "User not found"})
+  if (userIndex === -1) {
+    return res.status(404).json({ message: "User not found" })
   }
-  
+  const { nombre, mail, contrasena } = req.body
+  usuariosFicticios[userIndex] = {
+    ...usuariosFicticios[userIndex],
+    nombre,
+    mail,
+    contrasena
+  };
+  console.log("User updated successfully!!", usuariosFicticios[userIndex]);
+
   usuariosFicticios[userIndex] = {
     id: userId,
     nombre: req.body.nombre,
     mail: req.body.mail,
     contrasena: req.body.contrasena
   };
-  
+
   res.json(usuariosFicticios[userIndex])
 };
-
-export const deleteData = (req, res) => {
-  const userId = parseInt(req.params.id);
-  res.json ({message: "usuario con id: " + userId + " eliminado"});
-}
-
